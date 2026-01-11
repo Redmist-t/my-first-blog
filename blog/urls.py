40962b5166be503b390e465f302cf0ec1da9fde2
Redmist <irala0527@gmail.com>
2026-01-10 22:17:34 -0300

    Agg Detalle

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('detalle', views.detalle_list, name='detalle.html'), # primer termino va nombre que uso en navegador, segundo termino va la funcion que uso para esa vista, nombre archivo html
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+def detalle_list(request):
+    return render(request, 'blog/detalle.html', {})
+
+def detalle_ejemplo(request):
+    return render(request, 'blog/ejemplo.html', {})
