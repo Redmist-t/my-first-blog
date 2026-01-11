d1b6030caccc04fc3eaf9043b88028f6dd3df221
Redmist <irala0527@gmail.com>
2026-01-10 21:25:43 -0300

    Se arreglan URLS

--- misito/settings.py
+++ misito/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--cv*5i^(0egw1&f^a*7=c!m3r&jhghx71m)&p^v3+kt_!6v5zg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
